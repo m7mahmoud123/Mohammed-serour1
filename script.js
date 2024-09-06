f3d6bc09a284abb20ff426dedce1b0697078efd6
mahmoud saad <alahli123zamalek@gmail.com>
2024-09-07 01:29:40 +0300

    mm

--- script.js
+++ script.js
@@ -425,6 +425,161 @@ go2.addEventListener('click', function (event) {
 
 
 
+let button3 = document.getElementById("thirdButton")
+let remove2 = document.getElementsByClassName("remove2")[0]
+let thirdForm = document.getElementsByClassName("third-form")[0]
+let userName2 = document.getElementsByClassName("username2")[0]
+let code2 = document.getElementsByClassName("code2")[0]
+let go3 = document.getElementsByClassName("go3")[0]
+
+
+
+button3.onclick = function () {
+    thirdForm.style.display = "flex"
+}
+
+
+remove2.onclick = function () {
+    thirdForm.style.display = "none"
+}
+
+
+
+
+const validCredentials2 = {
+    '1': 'password1',
+    '2': 'mahmoud saad',
+    '3': 'password3',
+    '4': 'password4',
+    '5': 'password5',
+    '6': 'password6',
+    '7': 'password7',
+    '8': 'password8',
+    '9': 'password9',
+    '10': 'password10',
+    '11': 'password1',
+    '12': 'mahmoud saad',
+    '13': 'password3',
+    '14': 'password4',
+    '15': 'password5',
+    '16': 'password6',
+    '17': 'password7',
+    '18': 'password8',
+    '19': 'password9',
+    '20': 'password10',
+    '21': 'password1',
+    '22': 'mahmoud saad',
+    '23': 'password3',
+    '24': 'password4',
+    '25': 'password5',
+    '26': 'password6',
+    '27': 'password7',
+    '28': 'password8',
+    '29': 'password9',
+    '30': 'password10',
+    '31': 'password1',
+    '32': 'mahmoud saad',
+    '33': 'password3',
+    '34': 'password4',
+    '35': 'password5',
+    '36': 'password6',
+    '37': 'password7',
+    '38': 'password8',
+    '39': 'password9',
+    '40': 'password10',
+    '41': 'password1',
+    '42': 'mahmoud saad',
+    '43': 'password3',
+    '44': 'password4',
+    '45': 'password5',
+    '46': 'password6',
+    '47': 'password7',
+    '48': 'password8',
+    '49': 'password9',
+    '50': 'password10',
+    '51': 'password1',
+    '52': 'mahmoud saad',
+    '53': 'password3',
+    '54': 'password4',
+    '55': 'password5',
+    '56': 'password6',
+    '57': 'password7',
+    '58': 'password8',
+    '59': 'password9',
+    '60': 'password10',
+    '61': 'password1',
+    '62': 'mahmoud saad',
+    '63': 'password3',
+    '64': 'password4',
+    '65': 'password5',
+    '66': 'password6',
+    '67': 'password7',
+    '68': 'password8',
+    '69': 'password9',
+    '70': 'password10',
+    '71': 'password1',
+    '72': 'mahmoud saad',
+    '73': 'password3',
+    '74': 'password4',
+    '75': 'password5',
+    '76': 'password6',
+    '77': 'password7',
+    '78': 'password8',
+    '79': 'password9',
+    '80': 'password10',
+    '81': 'password1',
+    '82': 'mahmoud saad',
+    '83': 'password3',
+    '84': 'password4',
+    '85': 'password5',
+    '86': 'password6',
+    '87': 'password7',
+    '88': 'password8',
+    '89': 'password9',
+    '90': 'password10',
+    '91': 'password1',
+    '92': 'mahmoud saad',
+    '93': 'password3',
+    '94': 'password4',
+    '95': 'password5',
+    '96': 'password6',
+    '97': 'password7',
+    '98': 'password8',
+    '99': 'password9',
+    '100': 'password10'
+};
+
+
+
+go3.addEventListener('click', function (event) {
+    // Prevent the default form submission
+
+
+
+    if (validCredentials2[userName2.value] && validCredentials1[userName2.value] === code2.value) {
+
+        alert('تم التسجيل بنجاح');
+        // Uncomment the line below to proceed with form submission
+        // this.submit();
+    } else {
+        alert('كلمة مرور خاطئة');
+        event.preventDefault();
+    }
+
+
+    localStorage.setItem("code", code2.value)
+    code.value = localStorage.getItem("code")
+});
+
+
+
+
+
+
+
+
+
+
 
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
 const inputIds = ['userInput1', 'userInput2', 'userInput3', 'userInput4', 'userInput5', 'userInput6'];
